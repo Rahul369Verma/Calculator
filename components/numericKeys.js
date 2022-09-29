6f0559e3a36ac9c917b711c45352028130f0d380
Rahul Verma <raj369verma@gmail.com>
2022-09-29 20:26:16 +0530

    deploy

--- components/numericKeys.js
+++ components/numericKeys.js
@@ -1,10 +1,30 @@
 /*eslint-disable*/
 
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const NumberKeys = (props) => {
 
-const { handleClick } = props;
+
+  const { handleClick } = props;
+
+  useEffect(() => {
+    const onScroll = e => {
+      // e.preventDefault();
+      console.log(e.key)
+      if (e.key === "0" || e.key === "1" || e.key === "2" || e.key === "3" || e.key === "4" || e.key === "5" || e.key === "6"
+        || e.key === "7" || e.key === "8" || e.key === "9" || e.key === ".") {
+        handleClick(e.key)
+      }
+    }
+    window.addEventListener("keydown", onScroll);
+    return function unMount() {
+      window.removeEventListener("keydown", onScroll);
+    };
+
+  }, [handleClick]);
+
+  
+
   return (
     <div className="numbers">
       <button type="button" data-testid="seven" className="btn-cal btn-primary" onClick={() => handleClick('7')}>7</button>
--- components/operatorkeys.js
+++ components/operatorkeys.js
@@ -1,8 +1,35 @@
 /*eslint-disable*/
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const OperatorKeys = (props) => {
   const { handleClick } = props;
+
+
+  useEffect(() => {
+    const onScroll = e => {
+      // e.preventDefault();
+      console.log(e.key)
+      if (e.key === "%" || e.key === "Backspace" || e.key === "Enter" || e.key === "/" ||
+        e.key === "*" || e.key === "-" || e.key === "+" || e.key === "=") {
+        if (e.key === "/") {
+          handleClick("÷")
+        } else if (e.key === "*") {
+          handleClick("x")
+        } else if (e.key === "Backspace") {
+          handleClick("CE")
+        } else if (e.key === "Enter") {
+          handleClick("=")
+        } else {
+          handleClick(e.key)
+        }
+      }
+    };
+    window.addEventListener("keydown", onScroll);
+    return function unMount() {
+      window.removeEventListener("keydown", onScroll);
+    };
+  }, [handleClick]); 
+
   return (
     <>
       <button type="button" className="btn-cal btn-top" onClick={() => handleClick('AC')}>AC</button>
