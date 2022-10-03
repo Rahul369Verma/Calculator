0beda84b2e8f92f207dc6632c295423e123cdee4
Rahul Verma <raj369verma@gmail.com>
2022-10-04 01:31:38 +0530

    test and deployed

--- pages/history/index.tsx
+++ pages/history/index.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { useEffect } from "react";
 import axios from "axios";
-import History from "../../components/History.js";
+import History from "../../components/History";
 
 const Home: NextPage = () => {
   return (
