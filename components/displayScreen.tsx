d8bcc3f0c9b4a0658e3765567f6dd2e105fb3d14
Rahul Verma <raj369verma@gmail.com>
2022-10-12 00:44:28 +0530

    antd Table

--- components/displayScreen.tsx
+++ components/displayScreen.tsx
@@ -1,18 +1,32 @@
 /*eslint-disable*/
-import React, { FC } from 'react';
+import React, { createRef, FC, useRef } from "react";
 
-const DisplayScreen: FC<{ total: string | null, next:  string | null, operation:  string | null}>  = (props) => {
+const DisplayScreen: FC<{
+  total: string | null;
+  next: string | null;
+  operation: string | null;
+}> = (props) => {
   const { total, next, operation } = props;
+  const ref = createRef<HTMLDivElement>();
+
+  React.useLayoutEffect(() => {
+    if (ref.current) {
+      if (ref?.current?.clientWidth < ref?.current?.scrollWidth) {
+        console.log("overflow");
+      }
+    }
+  }, [ref]);
+
   return (
-    <div className="displayscreen-cont">
-      <span className="display-screen">
-        {(!total && !operation && !next) && '0'}
+    <div className="displayscreen-cont" ref={ref}>
+      <span className="display-screen ps-2">
+        {!total && !operation && !next && "0"}
         {total?.toString()}
         {operation}
         {next?.toString()}
       </span>
     </div>
   );
-}
+};
 
 export default DisplayScreen;
